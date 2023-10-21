c248d109d6aecebe8c137b23a0d157a2fb065df6
Yaroslav Klimas <yarklim@gmail.com>
2023-10-21 11:50:17 +0300

    added register & login routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,17 +2,47 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { UserModel } from '../models/Users.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { JWT__SECRET } = process.env;
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
-
   const user = await UserModel.findOne({ username });
 
-  res.json(user);
+  if (user) {
+    return res.json({ message: 'User already exists!' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: 'User Registered Successfully!' });
 });
 
-router.post('/login');
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User Doesn't Exist!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: 'Username or Password is incorrect!' });
+  }
+
+  const token = jwt.sign({ id: user._id }, JWT__SECRET);
+
+  res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
